602dfc8ee046cf7ca09f46ec9810f6bf5bf3239e
armthananon <armza054@gmail.com>
2025-02-07 20:56:53 +0700

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/project-management-applications',
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
